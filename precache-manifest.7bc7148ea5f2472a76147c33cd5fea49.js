45c5e676fbc9fd2547a2dfc81cc49288be73584e
Shaike07 <shaikemathar04@gmail.com>
2022-04-21 16:08:50 +0530

    Updates

---
+++ precache-manifest.7bc7148ea5f2472a76147c33cd5fea49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1559d6a5c14ba69176a467d37494b18a",
+    "url": "/cric-plaza/index.html"
+  },
+  {
+    "revision": "865614533c9e684d081b",
+    "url": "/cric-plaza/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "fc389d20e5df834a7e13",
+    "url": "/cric-plaza/static/css/main.c1ad2426.chunk.css"
+  },
+  {
+    "revision": "865614533c9e684d081b",
+    "url": "/cric-plaza/static/js/2.6f2f86c8.chunk.js"
+  },
+  {
+    "revision": "fc389d20e5df834a7e13",
+    "url": "/cric-plaza/static/js/main.a4ef82ef.chunk.js"
+  },
+  {
+    "revision": "352b1d154b6a273f7b59",
+    "url": "/cric-plaza/static/js/runtime~main.e99edd5b.js"
+  }
+]);
